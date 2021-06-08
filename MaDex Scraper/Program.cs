4ae3b75b643cd17b1b6c5a88310a668b8d2407ca
Andrey Goldstein <brucemarley123@gmail.com>
2021-06-08 04:05:22 -0400

    basic ui fixes

--- MaDex Scraper/Program.cs
+++ MaDex Scraper/Program.cs
@@ -19,6 +19,7 @@ namespace MaDex_Scraper
             while (true)
             {
                 Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("Welcome to Jabarabo's MangaDex Scraper.");
                 Console.WriteLine("--------------------------------------");
                 Console.WriteLine("Please paste manga ID. This is the large line of alphanumerical characters in the URL for the manga.");
@@ -30,7 +31,8 @@ namespace MaDex_Scraper
                 
                 
                 Console.Clear();
-                Console.WriteLine("What chapter would you like to download?");
+                Console.WriteLine("Enter the number of the chapter you would like to download?");
+                Console.WriteLine("Ex. 46");
                 string MangaChapterNum = Console.ReadLine();
                 
                 
@@ -44,62 +46,64 @@ namespace MaDex_Scraper
                 
                 List<JToken> EnglishChapters = new List<JToken>();
                 foreach (JToken jtoken in JSON["results"])
-                {
-                    if (jtoken["data"]["attributes"]["translatedLanguage"].Value<string>() == "en")
-                       EnglishChapters.Add(jtoken);
-                };
+                    {
+                        if (jtoken["data"]["attributes"]["translatedLanguage"].Value<string>() == "en")
+                           EnglishChapters.Add(jtoken);
+                    };
                 if (EnglishChapters.Count == 0)
-                {
-                    Console.Clear();
-                    Console.WriteLine("There are no english chapters available. We don't support any language other than english. I'm too lazy to add support.");
-                    Console.WriteLine("Cry More\n\n");
-                    Console.WriteLine("Press any key to continue.");
-                    Console.ReadKey();
-                }
+                    {
+                        Console.Clear();
+                        Console.WriteLine("There are no english chapters available. We don't support any language other than english. I'm too lazy to add support.");
+                        Console.WriteLine("Cry More\n\n");
+                        Console.WriteLine("Press any key to continue.");
+                        Console.ReadKey();
+                    }
                 else if (EnglishChapters.Count == 1)
-                {
-                    JToken chapter = EnglishChapters[0];
-                    string hash = chapter["data"]["attributes"]["hash"].Value<string>();
-                    List<string> pageURLs = new List<string>();
-                    foreach (JToken jtoken in chapter["data"]["attributes"]["data"])
                     {
-                        pageURLs.Add($"https://uploads.mangadex.org/data/{hash}/{jtoken.Value<string>()}");
-                    };
+                        JToken chapter = EnglishChapters[0];
+                        string hash = chapter["data"]["attributes"]["hash"].Value<string>();
+                        List<string> pageURLs = new List<string>();
+                        foreach (JToken jtoken in chapter["data"]["attributes"]["data"])
+                            {
+                                pageURLs.Add($"https://uploads.mangadex.org/data/{hash}/{jtoken.Value<string>()}");
+                            };
 
 
-                    Console.Clear();
-                    Console.WriteLine("What would you like to name the file?");
-                    string PDFName = Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("What would you like to name the file?");
+                        string PDFName = Console.ReadLine(); //Names the PDF file
 
 
-                    Console.Clear();
-                    Console.WriteLine("Where would you like to save the PDF to?");
-                    FolderBrowserDialog NewOutputDir = new FolderBrowserDialog(); //Creates the object for finding the folder.
-                    string Outpath = "big monkey nuts";
-                    if (NewOutputDir.ShowDialog() == System.Windows.Forms.DialogResult.OK) //Opens dialog and user selects new output folder.
-                    {
-                        Outpath = NewOutputDir.SelectedPath;
-                    };
-                    string path = $"{Outpath}\\{PDFName}.pdf";
+                        Console.Clear();
+                        Console.WriteLine("Where would you like to save the PDF to?");
+                        FolderBrowserDialog NewOutputDir = new FolderBrowserDialog(); //Creates the object for finding the folder.
+                        string Outpath = "big monkey nuts";
+                        if (NewOutputDir.ShowDialog() == System.Windows.Forms.DialogResult.OK) //Opens dialog and user selects new output folder.
+                            {
+                                Outpath = NewOutputDir.SelectedPath;
+                            };
+                        string path = $"{Outpath}\\{PDFName}.pdf";
 
 
-                    Document document = new Document();
-                    PdfWriter.GetInstance(document, new FileStream(path, FileMode.Create));
-                    document.Open();
-                    foreach (string url in pageURLs)
-                    {
-                        iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(url);
-                        document.SetMargins(0, 0, 0, 0);
-                        image.ScaleToFit(document.PageSize);
-                        document.Add(image);
-                    };
-                    document.Close();
-                    Console.Clear();
-                }
+                        Document document = new Document();
+                        PdfWriter.GetInstance(document, new FileStream(path, FileMode.Create));
+                        document.Open();
+                        foreach (string url in pageURLs)
+                            {
+                                iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(url);
+                                document.SetMargins(0, 0, 0, 0);
+                                image.ScaleToFit(document.PageSize);
+                                document.Add(image);
+                            };
+                        document.Close();
+                        Console.Clear();
+                    }
                 else
-                {
-                    Console.WriteLine("We don't support multiple translators. It's their fault for being so damn prolific.");
-                }
+                    {
+                        Console.WriteLine("We don't support multiple translators. It's their fault for being so damn prolific.");
+                        Console.WriteLine("Press any key to continue");
+                        Console.ReadKey();
+                    }
             }
         }
     }
